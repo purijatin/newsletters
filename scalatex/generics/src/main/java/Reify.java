00ba6a1112e1bcaf3286b7f669deab9a7a17cb70
puri <jatin.puri@deshaw.com>
2015-04-28 05:13:44 -0400

    ome more info

--- scalatex/generics/src/main/java/Reify.java
+++ scalatex/generics/src/main/java/Reify.java
@@ -3,13 +3,13 @@ public class Reify {
     public void example1(){
         //start
         Dog[] d = new Dog[1];
-        d.getClass().equals(Dog[].class);//It knows it is an array
+        d.getClass().equals(Dog[].class);//It knows it is an array of dogs
         Object[] ob = d;
-        ob[0] = new Animal(); //throws ArrayStoreException. This is because it has information available at runtime regarding dogness of `d`
+        ob[0] = new Animal(); //throws ArrayStoreException at runtime. This is because it has information available at runtime regarding dogness of `d`
         //end
     }
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Reify r = new Reify();
         r.example1();
     }
